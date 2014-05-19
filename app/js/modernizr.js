cc7c48353a6a25b0ffdde047562441c8416ac593
jlalovi <jlalovi@gmail.com>
2014-05-19 14:50:28 +0200

    SHIPMAP-12 ie cursor on map has a dragging pointer

--- app/js/modernizr.js
+++ app/js/modernizr.js
@@ -8,7 +8,7 @@
 
 window.Modernizr = (function( window, document, undefined ) {
 
-    var version = '2.7.1',
+    var version = '2.8.0',
 
     Modernizr = {},
 
@@ -91,7 +91,7 @@ window.Modernizr = (function( window, document, undefined ) {
 
       var matchMedia = window.matchMedia || window.msMatchMedia;
       if ( matchMedia ) {
-        return matchMedia(mq).matches;
+        return matchMedia(mq) && matchMedia(mq).matches || false;
       }
 
       var bool;
